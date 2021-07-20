9adbd57d859a0367c79906db8867b180b9b98b22
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-07-20 11:47:10 -0700

    v.003

--- light/settings.py
+++ light/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import os
 from decouple import config
-
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
